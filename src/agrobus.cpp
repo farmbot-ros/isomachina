1193155e1b4af60633458fb0e0bd92fa77727dbb
Trim Bresilla <trim.bresilla@gmail.com>
2025-01-12 10:00:09 +0100

    feat: basic agrostack++ integration

--- src/agrobus.cpp
+++ src/agrobus.cpp
@@ -1,3 +1,6 @@
-int main(){
-  return 0;
-}
+#include "isobus/hardware_integration/can_hardware_interface.hpp"
+#include "isobus/hardware_integration/socket_can_interface.hpp"
+#include "isobus/isobus/can_network_manager.hpp"
+#include "isobus/isobus/can_partnered_control_function.hpp"
+
+int main() { return 0; }
